f8730bca5c7224f6c325d8d69c9ed5457ec12105
wembleyWilliams <wembleywilliamsdev@gmail.com>
2021-10-27 17:15:13 -0500

    GG-API-0005 Disabled CORS to allow localhost connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import router from "./routes";
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const log = require('loglevel');
 log.setDefaultLevel("INFO")
 
@@ -8,6 +9,7 @@ const app = express();
 require('dotenv').config()
 app.use(bodyParser.json())
 app.use(router)
+app.use(cors({origin: false}))
 
 app.listen(process.env.PORT, () => {
     log.info(`Server started on port ${process.env.PORT}` )
