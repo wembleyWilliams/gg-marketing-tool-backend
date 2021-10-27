616c071f46179dfb0a2524a3c959fc3cb40eb3e2
wembleyWilliams <wembleywilliamsdev@gmail.com>
2021-10-27 18:25:03 -0500

    GG-API-0006 handling cors errors

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,21 @@ log.setDefaultLevel("INFO")
 
 const app = express();
 require('dotenv').config()
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(router)
-app.use(cors({origin: "*"}))
+app.use((req: any, res: any, next: any) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin',
+        'X-Requested-With', 'Content-Type','Accept',
+        'Authorization'
+    )
+    if(req.method==='OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,GET,DELETE')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.listen(process.env.PORT, () => {
     log.info(`Server started on port ${process.env.PORT}` )
