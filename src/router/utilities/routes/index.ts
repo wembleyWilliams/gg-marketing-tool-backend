07787e371de8c42d17c5adddc12c00e4c3dad644
wembleyWilliams <wembleywilliamsdev@gmail.com>
2024-09-19 09:55:02 -0500

    patch: Updated the getVCard to look for "ownerId", previous version caused an error looking for userId that was deprecated

--- src/router/utilities/routes/index.ts
+++ src/router/utilities/routes/index.ts
@@ -6,14 +6,14 @@ log.setDefaultLevel("INFO")
 
 export const getVCard = async (req: Request, res: Response) => {
 
-    let vCard = await generateContactCard(req.body.userId)
+    let vCard = await generateContactCard(req.body.ownerId)
         .then((res)=>{
             return res
             })
 
     //set content-type and disposition including desired filename
-    res.set('Content-Type', `text/vcard; name="${req.body.userId}.vcf"`);
-    res.set('Content-Disposition', `inline; filename="${req.body.userId}.vcf"`);
+    res.set('Content-Type', `text/vcard; name="${req.body.ownerId}.vcf"`);
+    res.set('Content-Disposition', `inline; filename="${req.body.ownerId}.vcf"`);
 
     //send the response
     console.log('Sending VCard')
