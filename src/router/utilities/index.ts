0e842e277df0e22396d70d8c0d65aaef377fd373
wembleyWilliams <wembleywilliamsdev@gmail.com>
2024-10-01 09:45:10 -0500

    chore: CARD-11: Created crud endpoints for VCARD

--- src/router/utilities/index.ts
+++ src/router/utilities/index.ts
@@ -1,6 +1,13 @@
-import {getVCard} from "./routes";
+import {createVCard, deleteVCard, getVCard, updateVCard} from "./routes";
+
 const express = require('express')
 const utility = express.Router()
+
+utility.delete('/contact-card/delete', deleteVCard)
+
+utility.post('/contact-card/create', createVCard)
+
+utility.put('/contact-card/update', updateVCard)
 
 utility.post('/contact-card', getVCard)
 
